429d2af9bd2b2eb3a1be7eb4d6d303d0561b804d
samugarciar <samugarciaro@outlook.com>
2026-05-30 08:13:51 -0500

    arreglo deployment

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.10", "localhost:3000"],
-  serverActions: {
-    bodySizeLimit: "5mb",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
   },
 };
 
